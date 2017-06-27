158962c3e824b4837d5dec53d36f997abc58d2b8
Tom <traggett@googlemail.com>
2017-06-27 18:37:24 +0100

    updated SceneUtils

--- Framework/Utils/Assets/SceneUtils.cs
+++ Framework/Utils/Assets/SceneUtils.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -62,6 +63,18 @@ namespace Framework
 				}
 
 				return component;
+			}
+
+			public static T[] FindAllInScene<T>(Scene scene) where T : Component
+			{
+				List<T> components = new List<T>();
+
+				foreach (GameObject rootObject in scene.GetRootGameObjects())
+				{
+					components.AddRange(rootObject.GetComponentsInChildren<T>());
+				}
+
+				return components.ToArray();
 			}
 		}
 	}
