71d1a0fc1b64b29187763a4d73cb1ed7b8a433ad
Tom <traggett@googlemail.com>
2017-11-13 14:20:56 +0000

    Updated PlayModeComponentSaver

--- Framework/Utils/PlayModeComponentSaver/Editor/UnityPlayModeSaver.cs
+++ Framework/Utils/PlayModeComponentSaver/Editor/UnityPlayModeSaver.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 using System.Reflection;
@@ -14,40 +14,78 @@ namespace Framework
 			[InitializeOnLoad]
 			public static class UnityPlayModeSaver
 			{
-				#region Strings
+				#region Constants
 				private const string kSaveComponentMenuString = "CONTEXT/Component/Save Values To Scene";
-				private const string kSaveGameObjectMenuString = "GameObject/Save Game Object";
-				private const string kUndoText = "Set values from Play Mode";
-
+				private const string kRevertComponentMenuString = "CONTEXT/Component/Revert Play Mode Changes";
+				private const string kUndoText = "Set Play Mode Values";
 				private const string kEditorPrefRoot = "UnityPlayModeSaver.";
-				private const string kEditorObjects = kEditorPrefRoot + "SavedObjects";
+				private const string kEditorSaveObjectsCount = kEditorPrefRoot + "SavedObjects";
+				private const char kSplitChar = '?';
+				private const string kIdentifierProperty = "m_LocalIdentfierInFile";   //note the misspelling!
+				private const string kInspectorProperty = "inspectorMode"; 
 				#endregion
 
+				#region Constructor
 				static UnityPlayModeSaver()
 				{
 					EditorApplication.playModeStateChanged += ModeChanged;
 				}
+				#endregion
 
 				#region Menus
-				[MenuItem(kSaveComponentMenuString)]
+				[MenuItem(kSaveComponentMenuString, false, 12)]
 				private static void SaveComponent(MenuCommand command)
 				{
-					Component component = (Component)command.context;
+					Component component = command.context as Component;
 
 					if (Application.isPlaying && component != null)
 					{
-						SaveObject(component);
+						SaveComponent(component);
 					}
 				}
 
 				[MenuItem(kSaveComponentMenuString, true)]
 				private static bool ValidateSaveComponent(MenuCommand command)
 				{
-					Component component = (Component)command.context;
-					return Application.isPlaying && component != null;
+					Component component = command.context as Component;
+
+					return Application.isPlaying && component != null && GetIdentifier(component) != -1;
+				}
+
+				[MenuItem(kRevertComponentMenuString, false, 12)]
+				private static void RevertComponent(MenuCommand command)
+				{
+					Component component = command.context as Component;
+
+					if (Application.isPlaying && component != null)
+					{
+						RevertComponent(component);
+					}
+				}
+
+				[MenuItem(kRevertComponentMenuString, true)]
+				private static bool ValidateRevertComponent(MenuCommand command)
+				{
+					Component component = command.context as Component;
+
+					if (Application.isPlaying && component != null)
+					{
+						int identifier = GetIdentifier(component);
+
+						if (identifier != -1)
+						{
+							string scenePath = component.gameObject.scene.path;
+
+							if (GetComponentSaveIndex(identifier, scenePath) != -1)
+								return true;
+						}
+					}
+
+					return false;
 				}
 				#endregion
 
+				#region Private Functions
 				private static void ModeChanged(PlayModeStateChange state)
 				{
 					switch (state)
@@ -62,106 +100,164 @@ namespace Framework
 
 				private static void RestoreSavedObjects()
 				{
-					string savedObjects = EditorPrefs.GetString(kEditorObjects);
+					int numSavedObjects = EditorPrefs.GetInt(kEditorSaveObjectsCount, 0);
+					List<Object> restoredObjects = new List<Object>();
+					List<string> restoredObjectsJSONData = new List<string>();
 
-					if (!string.IsNullOrEmpty(savedObjects))
+					for (int i = 0; i < numSavedObjects; i++)
 					{
-						string[] objects = savedObjects.Split(',');
+						string objecKey = kEditorPrefRoot + System.Convert.ToString(i);
+						string objectStr = EditorPrefs.GetString(objecKey, "");
+						string jsonStr;
 
-						if (objects != null && objects.Length > 0)
+						Component component = GetComponent(objectStr, out jsonStr);
+
+						if (component != null)
 						{
-							List<Object> restoredObjects = new List<Object>();
-							List<string> restoredObjectsData = new List<string>();
+							restoredObjects.Add(component);
+							restoredObjectsJSONData.Add(jsonStr);
+						}
 
-							for (int i = 0; i < objects.Length; i++)
-							{
-								try
-								{
-									int objectId = System.Convert.ToInt32(objects[i]);
-									Object obj = GetObject(objectId);
+						EditorPrefs.DeleteKey(objecKey);
+					}
 
-									if (obj != null && !restoredObjects.Contains(obj))
-									{
-										restoredObjects.Add(obj);
-										restoredObjectsData.Add(EditorPrefs.GetString(kEditorPrefRoot + objects[i]));
-									}
+					if (restoredObjects.Count > 0)
+					{
+						Undo.RecordObjects(restoredObjects.ToArray(), kUndoText);
 
-									EditorPrefs.DeleteKey(kEditorPrefRoot + objects[i]);
-								}
-								catch { }
-							}
-
-							if (restoredObjects.Count > 0)
-							{
-								Undo.RecordObjects(restoredObjects.ToArray(), kUndoText);
-
-								for (int i = 0; i < restoredObjects.Count; i++)
-								{
-									if (UseEditorSerialiser(restoredObjects[i]))
-										EditorJsonUtility.FromJsonOverwrite(restoredObjectsData[i], restoredObjects[i]);
-									else
-										JsonUtility.FromJsonOverwrite(restoredObjectsData[i], restoredObjects[i]);
-								}
-							}
+						for (int i = 0; i < restoredObjects.Count; i++)
+						{
+							if (UseEditorSerialiser(restoredObjects[i]))
+								EditorJsonUtility.FromJsonOverwrite(restoredObjectsJSONData[i], restoredObjects[i]);
+							else
+								JsonUtility.FromJsonOverwrite(restoredObjectsJSONData[i], restoredObjects[i]);
 						}
 					}
 
-					EditorPrefs.DeleteKey(kEditorObjects);
+					EditorPrefs.DeleteKey(kEditorSaveObjectsCount);
 				}
 
-				private static void SaveObject(Object obj)
+				private static Component GetComponent(string objectKey, out string jsonStr)
 				{
-					int identifier = GetIdentifier(obj);
+					string sceneStr, identifierStr;
 
-					if (identifier != -1)
+					if (GetComponentStrings(objectKey, out sceneStr, out identifierStr, out jsonStr))
 					{
-						string savedObjects = EditorPrefs.GetString(kEditorObjects);
-						string identifierStr = System.Convert.ToString(identifier);
+						int identifier = SafeConvertToInt(identifierStr);
+						Scene scene = SceneManager.GetSceneByPath(sceneStr);
 
-						if (!string.IsNullOrEmpty(savedObjects))
+						if (scene.IsValid())
 						{
-							List<string> objects = new List<string>(savedObjects.Split(','));
-
-							if (!objects.Contains(identifierStr))
+							foreach (GameObject rootObject in scene.GetRootGameObjects())
 							{
-								savedObjects += "," + identifierStr;
+								Component component = CheckComponents(rootObject, identifier);
+
+								if (component != null)
+								{
+									return component;
+								}
 							}
-						}
-						else
-						{
-							savedObjects = identifierStr;
-						}
-						
-						EditorPrefs.SetString(kEditorObjects, savedObjects);
-
-						string json;
-						
-						if (UseEditorSerialiser(obj))
-							json = EditorJsonUtility.ToJson(obj);
-						else
-							json = JsonUtility.ToJson(obj);
-
-						EditorPrefs.SetString(kEditorPrefRoot + identifier, json);
-					}
-				}
-
-				private static Object GetObject(int identifier)
-				{
-					Scene scene = SceneManager.GetActiveScene();
-
-					if (scene != null)
-					{
-						//Find object by id in scene
-						foreach (GameObject rootObject in scene.GetRootGameObjects())
-						{
-							Object obj = CheckGameObject(rootObject, identifier);
-
-							if (obj != null)
-								return obj;
 						}
 					}
 
 					return null;
+				}
+
+				private static bool GetComponentStrings(string objectKey, out string sceneStr, out string identifierStr, out string jsonStr)
+				{
+					sceneStr = null;
+					identifierStr = null;
+					jsonStr = null;
+
+					if (!string.IsNullOrEmpty(objectKey))
+					{
+						int split = objectKey.IndexOf(kSplitChar);
+
+						if (split != -1)
+						{
+							sceneStr = objectKey.Substring(0, split);
+
+							string idAndJson = objectKey.Substring(split + 1, objectKey.Length - split - 1);
+							split = idAndJson.IndexOf(kSplitChar);
+
+							if (split != -1)
+							{
+								identifierStr = idAndJson.Substring(0, split);
+								jsonStr = idAndJson.Substring(split + 1, idAndJson.Length - split - 1);
+								return true;
+							}
+						}
+					}	
+
+					return false;
+				}
+
+				private static int GetComponentSaveIndex(int identifier, string scenePath)
+				{
+					int numSavedObjects = EditorPrefs.GetInt(kEditorSaveObjectsCount, 0);
+
+					for (int i=0; i<numSavedObjects; i++)
+					{
+						string objectStr = EditorPrefs.GetString(kEditorPrefRoot + System.Convert.ToString(i));
+						string sceneStr, identifierStr, jsonStr;
+						
+						if (GetComponentStrings(objectStr, out sceneStr, out identifierStr, out jsonStr))
+						{
+							if (sceneStr == scenePath && identifier == SafeConvertToInt(identifierStr))
+							{
+								return i;
+							}
+						}
+					}
+
+					return -1;
+				}
+
+				private static void RevertComponent(Component component)
+				{
+					int identifier = GetIdentifier(component);
+
+					if (identifier != -1)
+					{
+						string scenePath = component.gameObject.scene.path;
+						int saveObjIndex = GetComponentSaveIndex(identifier, scenePath);
+
+						if (saveObjIndex != -1)
+						{
+							string objecKey = kEditorPrefRoot + System.Convert.ToString(saveObjIndex);
+							EditorPrefs.DeleteKey(objecKey);
+						}
+					}
+				}
+
+				private static void SaveComponent(Component component)
+				{
+					int identifier = GetIdentifier(component);
+
+					if (identifier != -1)
+					{
+						string scenePath = component.gameObject.scene.path;
+
+						int saveObjIndex = GetComponentSaveIndex(identifier, scenePath);
+
+						//If the component isn't already in saved object list add it
+						if (saveObjIndex == -1)
+						{
+							int objectCount = EditorPrefs.GetInt(kEditorSaveObjectsCount);
+							saveObjIndex = objectCount;
+							objectCount++;
+							EditorPrefs.SetInt(kEditorSaveObjectsCount, objectCount);
+						}
+
+						string json;
+
+						if (UseEditorSerialiser(component))
+							json = EditorJsonUtility.ToJson(component);
+						else
+							json = JsonUtility.ToJson(component);
+
+						EditorPrefs.SetString(kEditorPrefRoot + System.Convert.ToString(saveObjIndex), scenePath + kSplitChar + System.Convert.ToString(identifier) + kSplitChar + json);
+					}
 				}
 
 				private static bool UseEditorSerialiser(Object obj)
@@ -172,12 +268,8 @@ namespace Framework
 					return false;
 				}
 
-				private static Object CheckGameObject(GameObject gameObject, int identifier)
+				private static Component CheckComponents(GameObject gameObject, int identifier)
 				{
-					//Check actual gameObject
-					if (GetIdentifier(gameObject) == identifier)
-						return gameObject;
-
 					//Check components
 					Component[] components = gameObject.GetComponents<Component>();
 
@@ -190,33 +282,49 @@ namespace Framework
 					//Check children
 					foreach (Transform child in gameObject.transform)
 					{
-						Object obj = CheckGameObject(child.gameObject, identifier);
+						Component component = CheckComponents(child.gameObject, identifier);
 
-						if (obj != null)
-							return obj;
+						if (component != null)
+							return component;
 					}
 
 					return null;
 				}
 
-				private static int GetIdentifier(Object obj)
+				private static int GetIdentifier(Component component)
 				{
-					if (obj == null)
+					if (component != null)
 					{
-						return -1;
+						PropertyInfo inspectorModeInfo = typeof(SerializedObject).GetProperty(kInspectorProperty, BindingFlags.NonPublic | BindingFlags.Instance);
+
+						SerializedObject serializedObject = new SerializedObject(component);
+						inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+
+						SerializedProperty localIdProp = serializedObject.FindProperty(kIdentifierProperty);
+
+						if (localIdProp != null && localIdProp.intValue != 0)
+							return localIdProp.intValue;
 					}
 
-					EditorUtility.SetDirty(obj);
-
-					PropertyInfo inspectorModeInfo = typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
-
-					SerializedObject serializedObject = new SerializedObject(obj);
-					inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
-
-					SerializedProperty localIdProp = serializedObject.FindProperty("m_LocalIdentfierInFile");   //note the misspelling!
-
-					return localIdProp.intValue;
+					return -1;
 				}
+
+				private static int SafeConvertToInt(string str)
+				{
+					int value = -1;
+
+					try
+					{
+						value = System.Convert.ToInt32(str);
+					}
+					catch
+					{
+
+					}
+
+					return value;
+				}
+				#endregion
 			}
 		}
 	}
