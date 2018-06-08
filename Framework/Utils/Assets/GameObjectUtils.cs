5ef39fd8ebe521ad433edbda51786c1c200d3150
Tom <traggett@googlemail.com>
2018-06-08 12:36:49 +0100

    math damping funcs

--- Framework/Utils/Assets/GameObjectUtils.cs
+++ Framework/Utils/Assets/GameObjectUtils.cs
@@ -1,19 +1,20 @@
 using Framework.Maths;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Framework
 {
 	namespace Utils
 	{
-		public static class GameObjectUtils
-		{
-			public static bool _isShuttingDown = false;
+        public static class GameObjectUtils
+        {
+            public static bool _isShuttingDown = false;
 
-			public static GameObject CreatePrefab(string resourceName)
-			{
-				GameObject prefabResource = Resources.Load(resourceName) as GameObject;
-				Object.DontDestroyOnLoad(prefabResource);
-				/*
+            public static GameObject CreatePrefab(string resourceName)
+            {
+                GameObject prefabResource = Resources.Load(resourceName) as GameObject;
+                Object.DontDestroyOnLoad(prefabResource);
+                /*
 				if (prefabResource == null)
 				{
 					throw new System.InvalidOperationException("Unable to load resource: " + resourceName);
@@ -23,182 +24,199 @@ namespace Framework
 					throw new System.InvalidOperationException("Loaded resource: " + resourceName);
 				}*/
 
-				GameObject prefab = SafeInstantiate(prefabResource, Vector3.zero, Quaternion.identity) as GameObject;
-				return prefab;
-			}
+                GameObject prefab = SafeInstantiate(prefabResource, Vector3.zero, Quaternion.identity) as GameObject;
+                return prefab;
+            }
 
-			public static T SafeInstantiate<T>(GameObject original, Vector3 position, Quaternion rotation) where T : Component
-			{
-				GameObject obj = SafeInstantiate(original, position, rotation);
+            public static T SafeInstantiate<T>(GameObject original, Vector3 position, Quaternion rotation) where T : Component
+            {
+                GameObject obj = SafeInstantiate(original, position, rotation);
 
-				if (obj != null)
-				{
-					return obj.GetComponent<T>();
-				}
+                if (obj != null)
+                {
+                    return obj.GetComponent<T>();
+                }
 
-				return null;
-			}
+                return null;
+            }
 
-			public static T SafeInstantiate<T>(GameObject original) where T : Component
-			{
-				return SafeInstantiate<T>(original, Vector3.zero, Quaternion.identity);
-			}
+            public static T SafeInstantiate<T>(GameObject original) where T : Component
+            {
+                return SafeInstantiate<T>(original, Vector3.zero, Quaternion.identity);
+            }
 
-			public static GameObject SafeInstantiate(GameObject original, Vector3 position, Quaternion rotation)
-			{
-				if (_isShuttingDown)
-					return null;  
-				else
-					return GameObject.Instantiate(original, position, rotation) as GameObject;
-			}
+            public static GameObject SafeInstantiate(GameObject original, Vector3 position, Quaternion rotation)
+            {
+                if (_isShuttingDown)
+                    return null;
+                else
+                    return GameObject.Instantiate(original, position, rotation) as GameObject;
+            }
 
-			public static GameObject SafeInstantiate(GameObject original)
-			{
-				if (_isShuttingDown)
-					return null;
-				else
-					return GameObject.Instantiate(original) as GameObject;
-			}
+            public static GameObject SafeInstantiate(GameObject original)
+            {
+                if (_isShuttingDown)
+                    return null;
+                else
+                    return GameObject.Instantiate(original) as GameObject;
+            }
 
-			public static void DeleteChildren(Transform transform, bool immediate = false)
-			{
-				for (int i = transform.childCount - 1; i >= 0; --i)
-				{
-					GameObject child = transform.GetChild(i).gameObject;
+            public static void DeleteChildren(Transform transform, bool immediate = false)
+            {
+                for (int i = transform.childCount - 1; i >= 0; --i)
+                {
+                    GameObject child = transform.GetChild(i).gameObject;
 
-					if (immediate)
-						GameObject.DestroyImmediate(child);
-					else
-						GameObject.Destroy(child);
-				}
-			}
+                    if (immediate)
+                        GameObject.DestroyImmediate(child);
+                    else
+                        GameObject.Destroy(child);
+                }
+            }
 
-			public static void ResetTransform(Transform transform)
-			{
-				transform.localPosition = Vector3.zero;
-				transform.localRotation = Quaternion.identity;
-				transform.localScale = Vector3.one;
-			}
+            public static void ResetTransform(Transform transform)
+            {
+                transform.localPosition = Vector3.zero;
+                transform.localRotation = Quaternion.identity;
+                transform.localScale = Vector3.one;
+            }
 
-			public static Matrix4x4 GetTransformMatrix(Transform transform)
-			{
-				return Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
-			}
+            public static Matrix4x4 GetTransformMatrix(Transform transform)
+            {
+                return Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+            }
 
-			public static Matrix4x4 GetLocalTransformMatrix(Transform transform)
-			{
-				return Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
-			}
+            public static Matrix4x4 GetLocalTransformMatrix(Transform transform)
+            {
+                return Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
+            }
 
-			public static void SetTransformFromMatrix(Transform transform, ref Matrix4x4 matrix)
-			{
-				transform.position = MathUtils.GetTranslationFromMatrix(ref matrix);
-				transform.rotation = MathUtils.GetRotationFromMatrix(ref matrix);
-				transform.localScale = MathUtils.GetScaleFromMatrix(ref matrix);
-			}
+            public static void SetTransformFromMatrix(Transform transform, ref Matrix4x4 matrix)
+            {
+                transform.position = MathUtils.GetTranslationFromMatrix(ref matrix);
+                transform.rotation = MathUtils.GetRotationFromMatrix(ref matrix);
+                transform.localScale = MathUtils.GetScaleFromMatrix(ref matrix);
+            }
 
-			public static void CopyTransform(Transform fromTransform, Transform toTransform, Space space = Space.World)
-			{
-				switch (space)
-				{
-					case Space.Self:
-						{
-							toTransform.localPosition = fromTransform.localPosition;
-							toTransform.localRotation = fromTransform.localRotation;
-							toTransform.localScale = fromTransform.localScale;
-						}
-						break;
-					case Space.World:
-						{
-							toTransform.position = fromTransform.position;
-							toTransform.rotation = fromTransform.rotation;
-							toTransform.localScale = fromTransform.localScale;
-						}
-						break;
-				}	
-			}
+            public static void CopyTransform(Transform fromTransform, Transform toTransform, Space space = Space.World)
+            {
+                switch (space)
+                {
+                    case Space.Self:
+                        {
+                            toTransform.localPosition = fromTransform.localPosition;
+                            toTransform.localRotation = fromTransform.localRotation;
+                            toTransform.localScale = fromTransform.localScale;
+                        }
+                        break;
+                    case Space.World:
+                        {
+                            toTransform.position = fromTransform.position;
+                            toTransform.rotation = fromTransform.rotation;
+                            toTransform.localScale = fromTransform.localScale;
+                        }
+                        break;
+                }
+            }
 
-			public static void SetLayerRecursively(GameObject obj, int newLayer)
-			{
-				if (null == obj)
-				{
-					return;
-				}
+            public static void SetLayerRecursively(GameObject obj, int newLayer)
+            {
+                if (null == obj)
+                {
+                    return;
+                }
 
-				obj.layer = newLayer;
+                obj.layer = newLayer;
 
-				foreach (Transform child in obj.transform)
-				{
-					if (null == child)
-					{
-						continue;
-					}
-					SetLayerRecursively(child.gameObject, newLayer);
-				}
-			}
+                foreach (Transform child in obj.transform)
+                {
+                    if (null == child)
+                    {
+                        continue;
+                    }
+                    SetLayerRecursively(child.gameObject, newLayer);
+                }
+            }
 
-			public static bool IsChildOf(Transform obj, Transform parent)
-			{
-				while (obj.parent != null)
-				{
-					if (obj.parent == parent)
-					{
-						return true;
-					}
+            public static bool IsChildOf(Transform obj, Transform parent)
+            {
+                while (obj.parent != null)
+                {
+                    if (obj.parent == parent)
+                    {
+                        return true;
+                    }
 
-					obj = obj.parent;
-				}
+                    obj = obj.parent;
+                }
 
-				return false;
-			}
+                return false;
+            }
 
-			public static string GetChildFullName(GameObject child, GameObject parent)
-			{
-				string name = child.name;
+            public static string GetChildFullName(GameObject child, GameObject parent)
+            {
+                string name = child.name;
 
-				if (child.transform != parent.transform)
-				{
-					Transform trans = child.transform.parent;
-					while (trans != null && trans != parent.transform)
-					{
-						name = trans.gameObject.name + "/" + name;
-						trans = trans.parent;
-					}
-				}			
+                if (child.transform != parent.transform)
+                {
+                    Transform trans = child.transform.parent;
+                    while (trans != null && trans != parent.transform)
+                    {
+                        name = trans.gameObject.name + "/" + name;
+                        trans = trans.parent;
+                    }
+                }
 
-				return name;
-			}
+                return name;
+            }
 
-			public static T GetComponent<T>(GameObject gameObject) where T : class
-			{
-				Component[] components = gameObject.GetComponents(typeof(Component));
+            public static T GetComponent<T>(GameObject gameObject) where T : class
+            {
+                Component[] components = gameObject.GetComponents(typeof(Component));
 
-				foreach (Component component in components)
-				{
-					T typedComponent = component as T;
+                foreach (Component component in components)
+                {
+                    T typedComponent = component as T;
 
-					if (typedComponent != null)
-						return typedComponent;
-				}
+                    if (typedComponent != null)
+                        return typedComponent;
+                }
 
-				return null;
-			}
+                return null;
+            }
 
-			public static T GetComponentInParents<T>(GameObject gameObject, bool includeInactive = false) where T : Component
-			{
-				while (gameObject != null)
-				{
-					Component[] components = gameObject.GetComponentsInParent(typeof(T), includeInactive);
+            public static T GetComponentInParents<T>(GameObject gameObject, bool includeInactive = false) where T : Component
+            {
+                while (gameObject != null)
+                {
+                    Component[] components = gameObject.GetComponentsInParent(typeof(T), includeInactive);
 
-					if (components != null && components.Length > 0)
-						return (T)components[0];
+                    if (components != null && components.Length > 0)
+                        return (T)components[0];
 
-					gameObject = gameObject.transform.parent != null ? gameObject.transform.parent.gameObject : null;
-				}
+                    gameObject = gameObject.transform.parent != null ? gameObject.transform.parent.gameObject : null;
+                }
 
-				return null;
-			}
+                return null;
+            }
 
-		}
+            public static Transform[] GetChildTransforms(Transform transform)
+            {
+                List<Transform> children = new List<Transform>();
+                AddChildTransforms(transform, ref children);
+                return children.ToArray();
+            }
+
+            private static void AddChildTransforms(Transform transform, ref List<Transform> transforms)
+            {
+                transforms.Add(transform);
+
+                foreach (Transform child in transform)
+                {
+                    AddChildTransforms(child, ref transforms);
+                }
+            }
+        }
+
 	}
 }
