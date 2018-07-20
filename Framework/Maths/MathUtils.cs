05332da28f7d2d8446d8e49afb1ae4c4454eecba
Tom <traggett@googlemail.com>
2018-07-20 01:37:53 +0100

    Added approx func

--- Framework/Maths/MathUtils.cs
+++ Framework/Maths/MathUtils.cs
@@ -296,6 +296,28 @@ namespace Framework
 					return false;
 				}
 			}
+
+			public static bool Approximately(float a, float b, float epsilon)
+			{
+				float absA = Mathf.Abs(a);
+				float absB = Mathf.Abs(b);
+				float diff = Mathf.Abs(a - b);
+				
+				if (a == b)
+				{ // shortcut, handles infinities
+					return true;
+				}
+				else if (a == 0 || b == 0 || diff < float.Epsilon)
+				{
+					// a or b is zero or both are extremely close to it
+					// relative error is less meaningful here
+					return diff < (epsilon * float.Epsilon);
+				}
+				else
+				{ // use relative error
+					return diff / Mathf.Min((absA + absB), float.Epsilon) < epsilon;
+				}
+			}
 		}
 	}
 }
