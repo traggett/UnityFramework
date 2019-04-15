1bfcb43859a64759ae0b0d8f9fa178e07d514f5d
Tom <traggett@googlemail.com>
2019-04-15 12:11:53 +0100

    more drawers

--- Framework/Maths/Editor/IntRangePropertyDrawer.cs
+++ Framework/Maths/Editor/IntRangePropertyDrawer.cs
@@ -10,6 +10,17 @@ namespace Framework
 			[CustomPropertyDrawer(typeof(IntRange))]
 			public class IntRangePropertyDrawer : RangePropertyDrawer<int>
 			{
+				public static IntRange FloatRangeField(Rect position, IntRange range, GUIContent label = null)
+				{
+					Rect labelPosition = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight);
+					EditorGUI.LabelField(labelPosition, label);
+
+					Rect minLabelPosition = new Rect(labelPosition.x + labelPosition.width, position.y, position.width - EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight);
+					int[] values = new int[] { range._min, range._max };
+					EditorGUI.MultiIntField(minLabelPosition, new GUIContent[] { new GUIContent("f"), new GUIContent("t") }, values);
+
+					return new IntRange(range._min, range._max);
+				}
 			}
 		}
 	}
