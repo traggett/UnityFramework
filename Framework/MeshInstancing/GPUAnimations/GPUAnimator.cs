29bb80560bd123a9644d2f3dc2faef34d02115c5
Tom Raggett <traggett@googlemail.com>
2019-06-12 15:00:22 +0100

    Updated animator

--- Framework/MeshInstancing/GPUAnimations/GPUAnimator.cs
+++ Framework/MeshInstancing/GPUAnimations/GPUAnimator.cs
@@ -19,12 +19,8 @@ namespace Framework
 				#region Private Data
 				private static Avatar _dummyAvatar;
 				private Animator _animator;
-				private SkinnedMeshRenderer _skinnedMeshRenderer;			
-				
-				private int _currentPlayerIndex;
-				private GPUAnimationPlayer[] _clipPlayers;
-				private int[] _clipPlayerStates;
-				private float _currentAnimationWeight;
+				private SkinnedMeshRenderer _skinnedMeshRenderer;
+				private GPUAnimatorLayer _baseLayer;
 				#endregion
 
 				#region MonoBehaviour
@@ -32,11 +28,7 @@ namespace Framework
 				{
 					_animator = GetComponent<Animator>();
 					_skinnedMeshRenderer = GameObjectUtils.GetComponent<SkinnedMeshRenderer>(this.gameObject, true);
-					_clipPlayers = new GPUAnimationPlayer[2];
-					_clipPlayerStates = new int[2];
-					_currentPlayerIndex = 0;
-					_currentAnimationWeight = 1.0f;
-
+					_baseLayer = new GPUAnimatorLayer(_animator, 0);
 					_onInitialise += Initialise;
 
 					UpdateCachedTransform();
@@ -55,17 +47,17 @@ namespace Framework
 				#region GPUAnimatorBase
 				public override float GetCurrentAnimationFrame()
 				{
-					return _clipPlayers[_currentPlayerIndex].GetCurrentTexureFrame();
+					return _baseLayer.GetCurrentAnimationFrame();
 				}
 
 				public override float GetCurrentAnimationWeight()
 				{
-					return _currentAnimationWeight;
+					return _baseLayer.GetCurrentAnimationWeight();
 				}
 
 				public override float GetPreviousAnimationFrame()
 				{
-					return _clipPlayers[1 - _currentPlayerIndex].GetCurrentTexureFrame();
+					return _baseLayer.GetPreviousAnimationFrame();
 				}
 
 				public override Bounds GetBounds()
@@ -80,97 +72,20 @@ namespace Framework
 				#region Private Functions
 				private void Initialise()
 				{
-					_clipPlayers[0] = new GPUAnimationPlayer();
-					_clipPlayers[1] = new GPUAnimationPlayer();
-					_currentPlayerIndex = 0;
-					_currentAnimationWeight = 1.0f;
-
 					_animator.runtimeAnimatorController = _renderer.GetOverrideControllerForAnimator(_animator);
 					_animator.avatar = GetDummyAvatar();
 				}
 				
-				private void PlayAnimation(AnimatorStateInfo state, AnimatorClipInfo[] clips, int playerIndex)
-				{
-					if (clips.Length > 0)
-					{
-						AnimationClip clip = clips[0].clip;
-						GPUAnimations.Animation animation = ((GPUAnimatorOverrideController)_animator.runtimeAnimatorController).GetAnimation(clip);
-						_clipPlayers[playerIndex] = new GPUAnimationPlayer(animation, animation._wrapMode, 0.0f);
-					}
-					else
-					{
-						_clipPlayers[playerIndex] = new GPUAnimationPlayer();
-					}
-				}
-
 				private void UpdateAnimator()
 				{
-					//Check if we're transitioning - TO DO! transition from state to same state seem broken / cause pops
-					if (_animator.IsInTransition(0))
-					{
-						AnimatorStateInfo previousState = _animator.GetCurrentAnimatorStateInfo(0);
-						AnimatorStateInfo nextState = _animator.GetNextAnimatorStateInfo(0);
-						AnimatorTransitionInfo transitionInfo = _animator.GetAnimatorTransitionInfo(0);
-
-						//Check current player is playing the next state
-						if (_clipPlayerStates[_currentPlayerIndex] != nextState.fullPathHash)
-						{
-							//If not switch current player index and start animation on player
-							_currentPlayerIndex = 1 - _currentPlayerIndex;
-							_clipPlayerStates[_currentPlayerIndex] = nextState.fullPathHash;
-
-							AnimatorClipInfo[] nextClips = _animator.GetNextAnimatorClipInfo(0);
-							PlayAnimation(nextState, nextClips, _currentPlayerIndex);
-						}
-
-						//Then check previous player is playing the previous state
-						if (_clipPlayerStates[1 - _currentPlayerIndex] != previousState.fullPathHash)
-						{
-							_clipPlayerStates[1 - _currentPlayerIndex] = previousState.fullPathHash;
-
-							AnimatorClipInfo[] previousClips = _animator.GetCurrentAnimatorClipInfo(0);
-							PlayAnimation(previousState, previousClips, 1 - _currentPlayerIndex);
-						}
-
-						//Update times
-						_clipPlayers[_currentPlayerIndex].SetNormalizedTime(nextState.normalizedTime, true);
-						_clipPlayers[1 - _currentPlayerIndex].SetNormalizedTime(previousState.normalizedTime, true);
-						_currentAnimationWeight = transitionInfo.normalizedTime;
-					}
-					//Otherwise just update current animation
-					else
-					{
-						AnimatorStateInfo currentState = _animator.GetCurrentAnimatorStateInfo(0);
-
-						if (_clipPlayerStates[_currentPlayerIndex] != currentState.fullPathHash)
-						{
-							_clipPlayerStates[_currentPlayerIndex] = currentState.fullPathHash;
-							AnimatorClipInfo[] currentClips = _animator.GetCurrentAnimatorClipInfo(0);
-							PlayAnimation(currentState, currentClips, _currentPlayerIndex);
-
-							//Stop other player
-							_clipPlayers[1 - _currentPlayerIndex] = new GPUAnimationPlayer();
-						}
-						
-						//Update time
-						_clipPlayers[_currentPlayerIndex].SetNormalizedTime(currentState.normalizedTime, true);
-						_currentAnimationWeight = 1.0f;
-					}
+					_baseLayer.Update();
 				}
 
 				private void UpdateRootMotion()
 				{
 					if (_animator.applyRootMotion)
 					{
-						_clipPlayers[_currentPlayerIndex].GetRootMotionVelocities(out Vector3 velocity, out Vector3 angularVelocity);
-
-						if (_currentAnimationWeight < 1.0f)
-						{
-							_clipPlayers[1 - _currentPlayerIndex].GetRootMotionVelocities(out Vector3 previousPlayerVelocity, out Vector3 previousPlayerAngularVelocity);
-
-							velocity = Vector3.Lerp(previousPlayerVelocity, velocity, _currentAnimationWeight);
-							velocity = Vector3.Lerp(previousPlayerAngularVelocity, angularVelocity, _currentAnimationWeight);
-						}
+						_baseLayer.GetRootMotionVelocities(out Vector3 velocity, out Vector3 angularVelocity);
 
 						Quaternion rotation = this.transform.localRotation;
 						Quaternion delta = Quaternion.Euler(angularVelocity * Time.deltaTime);
---
+++ Framework/MeshInstancing/GPUAnimations/GPUAnimatorLayer.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+
+namespace Framework
+{
+	namespace MeshInstancing
+	{
+		namespace GPUAnimations
+		{
+			public struct GPUAnimatorLayer
+			{
+				#region Private Data	
+				private Animator _animator;
+				private int _layer;
+				private int _currentPlayerIndex;
+				private GPUAnimationPlayer[] _clipPlayers;
+				private int[] _clipPlayerStates;
+				private float _currentAnimationWeight;
+				#endregion
+
+				#region Public Interface
+				public GPUAnimatorLayer(Animator animator, int layer)
+				{
+					_animator = animator;
+					_layer = layer;
+					_clipPlayers = new GPUAnimationPlayer[2];
+					_clipPlayerStates = new int[2];
+					_currentPlayerIndex = 0;
+					_currentAnimationWeight = 1.0f;
+				}
+
+				public float GetCurrentAnimationFrame()
+				{
+					return _clipPlayers[_currentPlayerIndex].GetCurrentTexureFrame();
+				}
+
+				public float GetCurrentAnimationWeight()
+				{
+					return _currentAnimationWeight;
+				}
+
+				public float GetPreviousAnimationFrame()
+				{
+					return _clipPlayers[1 - _currentPlayerIndex].GetCurrentTexureFrame();
+				}
+				
+				public void Update()
+				{
+					//Check if we're transitioning - TO DO! transition from state to same state seem broken / cause pops
+					if (_animator.IsInTransition(_layer))
+					{
+						AnimatorStateInfo previousState = _animator.GetCurrentAnimatorStateInfo(_layer);
+						AnimatorStateInfo nextState = _animator.GetNextAnimatorStateInfo(_layer);
+						AnimatorTransitionInfo transitionInfo = _animator.GetAnimatorTransitionInfo(_layer);
+
+						//Check current player is playing the next state
+						if (_clipPlayerStates[_currentPlayerIndex] != nextState.fullPathHash)
+						{
+							//If not switch current player index and start animation on player
+							_currentPlayerIndex = 1 - _currentPlayerIndex;
+							_clipPlayerStates[_currentPlayerIndex] = nextState.fullPathHash;
+
+							AnimatorClipInfo[] nextClips = _animator.GetNextAnimatorClipInfo(_layer);
+							PlayAnimation(nextState, nextClips, _currentPlayerIndex);
+						}
+
+						//Then check previous player is playing the previous state
+						if (_clipPlayerStates[1 - _currentPlayerIndex] != previousState.fullPathHash)
+						{
+							_clipPlayerStates[1 - _currentPlayerIndex] = previousState.fullPathHash;
+
+							AnimatorClipInfo[] previousClips = _animator.GetCurrentAnimatorClipInfo(_layer);
+							PlayAnimation(previousState, previousClips, 1 - _currentPlayerIndex);
+						}
+
+						//Update times
+						_clipPlayers[_currentPlayerIndex].SetNormalizedTime(nextState.normalizedTime, true);
+						_clipPlayers[1 - _currentPlayerIndex].SetNormalizedTime(previousState.normalizedTime, true);
+						_currentAnimationWeight = transitionInfo.normalizedTime;
+					}
+					//Otherwise just update current animation
+					else
+					{
+						AnimatorStateInfo currentState = _animator.GetCurrentAnimatorStateInfo(_layer);
+
+						if (_clipPlayerStates[_currentPlayerIndex] != currentState.fullPathHash)
+						{
+							_clipPlayerStates[_currentPlayerIndex] = currentState.fullPathHash;
+							AnimatorClipInfo[] currentClips = _animator.GetCurrentAnimatorClipInfo(_layer);
+							PlayAnimation(currentState, currentClips, _currentPlayerIndex);
+
+							//Stop other player
+							_clipPlayers[1 - _currentPlayerIndex] = new GPUAnimationPlayer();
+						}
+
+						//Update time
+						_clipPlayers[_currentPlayerIndex].SetNormalizedTime(currentState.normalizedTime, true);
+						_currentAnimationWeight = 1.0f;
+					}
+				}
+				
+				public void GetRootMotionVelocities(out Vector3 velocity, out Vector3 angularVelocity)
+				{
+					_clipPlayers[_currentPlayerIndex].GetRootMotionVelocities(out velocity, out angularVelocity);
+
+					if (_currentAnimationWeight < 1.0f)
+					{
+						_clipPlayers[1 - _currentPlayerIndex].GetRootMotionVelocities(out Vector3 previousPlayerVelocity, out Vector3 previousPlayerAngularVelocity);
+
+						velocity = Vector3.Lerp(previousPlayerVelocity, velocity, _currentAnimationWeight);
+						angularVelocity = Vector3.Lerp(previousPlayerAngularVelocity, angularVelocity, _currentAnimationWeight);
+					}
+				}
+				#endregion
+
+				#region Private Functions
+				private void PlayAnimation(AnimatorStateInfo state, AnimatorClipInfo[] clips, int playerIndex)
+				{
+					if (clips.Length > 0)
+					{
+						AnimationClip clip = clips[0].clip;
+						GPUAnimations.Animation animation = ((GPUAnimatorOverrideController)_animator.runtimeAnimatorController).GetAnimation(clip);
+						_clipPlayers[playerIndex] = new GPUAnimationPlayer(animation, animation._wrapMode, 0.0f);
+					}
+					else
+					{
+						_clipPlayers[playerIndex] = new GPUAnimationPlayer();
+					}
+				}
+				#endregion
+			}
+		}
+    }
+}
