645b257083103ccc439e853cabcbfcf90239348f
traggett <traggett@googlemail.com>
2020-10-06 16:37:22 +0100

    added ProcedualUIFade

---
+++ Framework/UI/ProcedualUIFade.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Framework
+{
+    namespace UI
+    {
+        public class ProcedualUIFade : MaskableGraphic
+        {
+            public Color _bottomColor;
+            public Color _topColor;
+
+            protected override void OnRectTransformDimensionsChange()
+            {
+                base.OnRectTransformDimensionsChange();
+                SetVerticesDirty();
+            }
+
+            protected override void OnPopulateMesh(VertexHelper vh)
+            {
+                vh.Clear();
+
+                var bottomLeftCorner = new Vector2(0, 0) - rectTransform.pivot;
+                bottomLeftCorner.x *= rectTransform.rect.width;
+                bottomLeftCorner.y *= rectTransform.rect.height;
+
+                CreateQuad(vh,
+                           bottomLeftCorner,
+                           bottomLeftCorner + (rectTransform.rect.width * Vector2.right) + (rectTransform.rect.height * Vector2.up));
+            }
+
+			private void CreateQuad(VertexHelper vertexHelper, Vector2 bottomLeftCorner, Vector2 topRightCorner)
+			{
+				var i = vertexHelper.currentVertCount;
+
+				UIVertex vert = new UIVertex();
+
+				vert.position = bottomLeftCorner;
+				vert.color = _bottomColor;
+				vertexHelper.AddVert(vert);
+
+				vert.position = new Vector2(topRightCorner.x, bottomLeftCorner.y);
+				vert.color = _bottomColor;
+				vertexHelper.AddVert(vert);
+
+				vert.position = topRightCorner;
+				vert.color = _topColor;
+				vertexHelper.AddVert(vert);
+
+				vert.position = new Vector2(bottomLeftCorner.x, topRightCorner.y);
+				vert.color = _topColor;
+				vertexHelper.AddVert(vert);
+
+				vertexHelper.AddTriangle(i + 0, i + 2, i + 1);
+				vertexHelper.AddTriangle(i + 3, i + 2, i + 0);
+			}
+
+		}
+	}
+}
